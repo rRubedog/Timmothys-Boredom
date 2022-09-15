07a8ca14dc7e86ece2bcd97bead5a7ad59c6a71d
RuBrain <89792821+rRubedog@users.noreply.github.com>
2022-09-15 05:50:23 +0000

    Added all (beta) main files, fixed css, and changed theme to edits/notes website.

--- script.js
+++ script.js
@@ -11,16 +11,30 @@ function saveEdits() {
 
     //write a confirmation to the user
     document.getElementById("update").innerHTML="Edits saved!";
-
+    document.getElementById("update").style.color = "lightgreen";
+    document.getElementById("update").style.textShadow = "green 0px 0px 20px";
     }
     function checkEdits() {
 
     //find out if the user has previously saved edits
-    if(localStorage.userEdits!=null)
+    if(localStorage.userEdits!=null){
       document.getElementById("edit").innerHTML=localStorage.userEdits;
+    } 
     }
 
 function newRequest() {
-    var newEdit = document.createElementById("edit");
-    newEdit.innerHTML=localStorage.userEdits;
-}
+        
+        var newEdit = document.createElement("div");
+        document.getElementById('newEdits').appendChild(newEdit);
+        newEdit.id = "edit";
+        newEdit.innerHTML = "<div contenteditable=true>"+ 
+          document.getElementById("edit").innerHTML; 
+          + "</div>";
+        newEdit.style.marginTop = "10px";
+      }
+
+function runAll() {
+        saveEdits();
+        newRequest(); 
+      }
+
